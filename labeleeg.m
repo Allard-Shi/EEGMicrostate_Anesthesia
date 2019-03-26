2aa006edb8b818e062b6cef5482811efd525e52c
allardshi <873846725@qq.com>
2019-03-26 10:48:37 +0800

    update version 1

---
+++ labeleeg.m
@@ -0,0 +1,109 @@
+function [ms_array] = labeleeg(eegdata, ms, lamda, b, varargin)
+%% Fitting eegdata raw data using microstates
+%  
+%  Inputs:  1) eegdata   : EEG data array (a*b*c matrix)
+%                          a - Number of electrodes  b - Number of time frame 
+%                          c - Number of epoches
+%           2) ms        : Microstates (a*b matrix)
+%                          a - Number of electrodes  b - Number of microstates
+%           3) lamda     : Penalty parameter for nonsmoothness
+%           4) b         : Window semi-length
+%           5) default   : tol (1e-6) 
+%                          corr_thrsh - correaltion threshold (0.00 - 1.00) 
+%
+%  Outputs: 1) ms_array  :Labeled Microstate EEG array (a*b matrix)
+%                         a -  Number of time frame  b - Number of epoches
+%
+%  Anthor: Soupee Li, Allard Wen Shi  
+%  Copyright (C) Soupee Li, Allard Shi from Xian Jiaotong University 
+
+%% Default Params
+if ~exist('tol'),
+    tol = 1e-6;
+end
+if ~exist('corr_thrsh'),
+    corr_thrsh = 0.50;
+end
+
+%% Initialize the info. and Validation 
+GFP = std(eegdata,1);                               % Recalculate GFP values
+[N_e, N_ms] = size(ms);                             % Number of electrodes and microstates
+[~,N_num,N_epoch] = size(eegdata);                  % Time frame and Number of epoches                      
+
+if size(eegdata) ~= N_e
+    fprintf('Number of electrodes in micorstates and EEG does not match!\n');
+end
+
+%% Find peak locations in eegdata and compute correlation of each microstate.
+for i = 1:N_epoch            
+    [~,locs] = findpeaks(double(GFP(:,:,i)));       % Find the peaks      
+    Locs{i} = locs';    
+    [Corr{i},C{i}] = max(compute_spatial_correlation(eegdata(:,locs,i),ms),[],2);
+end
+Ka = C;
+
+%% Smoothing Algorithms
+for n = 1:N_epoch
+    sigma_o = 0; sigma_u = 1000;
+    frame = length(Locs{n});
+    Nbkt = zeros(frame-2*b,N_ms);                   % Define window
+    Peak_map = eegdata(:,Locs{n},n);
+    e = 0;
+    for j = 1:frame
+        e = e + (Peak_map(:,j)'*Peak_map(:,j)-(ms(:,C{n}(j))'*Peak_map(:,j))^2);     
+    end
+    e = e/(frame*(N_e-1));
+    
+    while abs(sigma_o-sigma_u) > tol*sigma_u
+        sigma_o = sigma_u;
+        
+        % Compute Nbkt between a window whose length is 2b+1
+        for t = 1+b:frame-b
+            for k = 1:N_ms
+                Nbkt(t-b,k) = length(find(C{n}(t-b:t+b) == k));
+            end
+
+            % argmin(k)
+            S = zeros(N_ms,1);
+            for k = 1:N_ms
+                S(k) = (Peak_map(:,t)'*Peak_map(:,t)-(ms(:,k)'*Peak_map(:,t))^2)/(2*e*(N_e-1))-lamda*Nbkt(t-b,k);
+            end
+            [~,Min_index] = min(S);
+            Ka{n}(t) = Min_index; 
+        end
+        C{n} = Ka{n};
+
+        % Compute new sigma_u
+        sigma_u = 0;
+        for j = 1:frame
+            sigma_u = sigma_u + (Peak_map(:,j)'*Peak_map(:,j)-(ms(:,C{n}(j))'*Peak_map(:,j))^2);     
+        end
+        sigma_u = sigma_u/(frame*(N_e-1));
+    end
+    
+end
+
+%% Label process
+% for i = 1:N_epoch
+%     Loc_lowcorr = find(Corr{i} < corr_thrsh);
+%     C{i}(Loc_lowcorr) = 0;
+% end
+
+ms_array=zeros(N_num,N_epoch);                       % Initialize the labeled array
+for i = 1:N_epoch
+    inner_index = 1;
+    for j = 1:N_num
+        if j < floor(1 + Locs{i}(2)/2)               % Discard the first and last microstate arrays
+            ms_array(j,i) = 0;
+        elseif j > floor((N_num+Locs{i}(end -1))/2)
+            ms_array(j,i) = 0;
+        elseif inner_index < size(Locs{i},1) && j == floor((Locs{i}(inner_index) + Locs{i}(inner_index+1))/2)
+            ms_array(j,i) = C{i}(inner_index+1);
+            inner_index = inner_index + 1;
+        else
+            ms_array(j,i) = C{i}(inner_index);
+        end
+    end 
+end
+
+end
---
+++ main.m
@@ -0,0 +1,57 @@
+%% Microstates Calculation and Validation
+%
+%  This file is aimed to compute the GFP and microstates of EEG 
+%  The EEG data has been preprocessed.
+%
+%  Anthor: Allard Shi, Xian Jiaotong University 
+
+%% Main code
+clear;clc;
+filepath = '';
+resultpath = ''; 
+datainfo_name = '';
+load(strcat(filepath,datainfo_name));
+N_people = 20;                                             % Number of People
+Num_cluster = ; Loop = 100;                                % Set the maximum of the number of clusters
+R_square = zeros(N_people,Num_cluster);
+R_cv = zeros(Num_cluster,1);
+W = zeros(Num_cluster,1);
+MS = [];
+b = ; lamda =  ;                                           % Set smoothing params
+
+%% Load data and compute the EEG microstates
+iter = ;
+for m = 1:iter
+    for c =
+        for n = 1:N_people   
+		
+            % obtain the name of file in the same section
+            filename = strcat(datainfo(4*n-3,1),'.set'); 
+            eeg = pop_loadset(filename, filepath);
+            fprintf('Import People %d  Success!\n', n);
+            EEG = eeg.data;
+            Fs = eeg.srate; T = 1/Fs;                           % sampling frequency
+            N_p = eeg.pnts;                                     % Number of sampling points
+            N_e = eeg.nbchan;                                   % Number of electrodes
+            N_trial = eeg.trials;                               % Number of trials
+            E_Loc = eeg.chanlocs;                               % Location of electrodes  
+            GFP = std(EEG,1);                                   % Define Global field power (preprocessed mean = 0)
+            Kappa = [];
+
+            % Extract all GFP peak maps
+            Gamma{n} = zeros(N_trial, N_e, 4);
+            MS = [];
+            for i = 1:N_trial
+                [X,locs] = findpeaks(double(GFP(:,:,i)));
+                [Kappa_max,gamma,R_temp,R_cv_temp,W_temp,M_temp] = mkmeans95(EEG(:,locs,i), ,Loop,b,lamda);
+                Kappa{i} = Kappa_max;
+                MS = [MS,gamma];
+            end
+            K{n} = Kappa;
+            fprintf('People %d Finished!\n', n);
+        end
+		
+		% Second clustering
+		[L{c},G{c},R{c},R_cv(c,m),W(c,m),~] = mkmeans95(MS, ,Loop,b,lamda);
+    end
+end
