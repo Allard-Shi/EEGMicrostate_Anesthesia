88b5075a0438a87f71728f30f8e2556f27e9ca65
Wen Shi <Allard-Shi@users.noreply.github.com>
2020-03-04 22:44:34 +0800

    update 2.0

--- compute_spatial_correlation.m
+++ compute_spatial_correlation.m
@@ -8,7 +8,7 @@ function C = compute_spatial_correlation(tseries,ms)
 %
 % ouputs:   1)C        : Correlation    (M*N matrix)
 %                        M - number of time frame  N - number of microstate
-%   
+%  
 
 %% Spatial correlation
 [~,frame] = size(tseries);
@@ -17,6 +17,9 @@ C = zeros(frame,num_ms);
 for i = 1:num_ms
     for j = 1:frame
         C(j,i) = sum(tseries(:,j).*ms(:,i))/sqrt(sum(tseries(:,j).^2)*sum(ms(:,i).^2));
+		
+		% absolute correlation 
+        C(j,i) = abs(C(j,i));
     end
 end
 
