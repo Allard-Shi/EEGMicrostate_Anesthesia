967f6958227b7f48742422acd5cf6be3acc255bf
Wen Shi <Allard-Shi@users.noreply.github.com>
2020-07-25 20:10:14 +0800

    version 2.1
    clean code

--- main.m
+++ main.m
@@ -4,29 +4,35 @@
 %  The data used in the research has been avaliable in the following website.
 %  link: https://www.repository.cam.ac.uk/handle/1810/252736
 %
-%  Anthor: Allard Shi, Xian Jiaotong University 
+%  Anthor: Allard Wen Shi, Xian Jiaotong University 
 
 %% Main code
+% remember to load EEGLAB
 clear;clc;
 addpath('func');
-filepath = '';
+filepath = 'H:/Sedation-RestingState/';
 resultpath = ''; 
-datainfo_name = '';
-load(strcat(filepath,datainfo_name));
-N_people = 20;                                             % Number of People
-Num_cluster = ; Loop = 100;                                % Set the maximum of the number of clusters
+datainfo_name = 'datainfo';
+load([filepath,datainfo_name]);
+N_people = 1;                                             % Number of People
+Num_cluster = 5;                                          % Set the number of clusters
+Loop = 1000;                                              % Set loops in mkmeans 
 R_square = zeros(N_people,Num_cluster);
 R_cv = zeros(Num_cluster,1);
 W = zeros(Num_cluster,1);
 MS = [];
-b = 1; lamda = 0.05;                                           % Set smoothing params (smooth the microstate sequence)
+b = 1; lamda = 0;                                         % Set smoothing params (smooth the microstate sequence)
 
 %% Load data and compute the EEG microstates
+% seek for optical number of cluster
 iter = 1;
+
+% Microstate clustering
 for m = 1:iter
-    for c =
+    
+    % c is for test or other loops
+    for c = 1
         for n = 1:N_people   
-		
             % obtain the name of file in the same section
             filename = strcat(datainfo(4*n-3,1),'.set'); 
             eeg = pop_loadset(filename, filepath);
@@ -41,11 +47,10 @@ for m = 1:iter
             Kappa = [];
 
             % Extract all GFP peak maps
-            Gamma{n} = zeros(N_trial, N_e, 4);
             MS = [];
             for i = 1:N_trial
                 [X,locs] = findpeaks(double(GFP(:,:,i)));
-                [Kappa_max,gamma,R_temp,R_cv_temp,W_temp,M_temp] = mkmeans95(EEG(:,locs,i), ,Loop,b,lamda);
+                [Kappa_max,gamma,R_temp,R_cv_temp,W_temp,M_temp] = mkmeans95(EEG(:,locs,i),5,Loop,b,lamda);
                 Kappa{i} = Kappa_max;
                 MS = [MS,gamma];
             end
@@ -54,6 +59,9 @@ for m = 1:iter
         end
 		
 		% Second clustering
-		[L{c},G{c},R{c},R_cv(c,m),W(c,m),~] = mkmeans95(MS, ,Loop,b,lamda);
+		[L{c},G{c},R{c},R_cv(c,m),W(c,m),~] = mkmeans95(MS,5,Loop,b,lamda);
     end
+    
+    % visualize the microstate
+    ms_visualize(G{c},E_Loc)
 end
